0afed713fe6acba2dd82e5ff25148f4fd86fef76
lucasfontana05 <lfontanag@fi.uba.ar>
2026-06-12 14:58:14 -0300

    commit de prueba

--- consolidar.sh
+++ consolidar.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# comentario de prueba
+# a
+# b
+# c
+
 while true; do # este bucle va a ejecutarse permanentemente (hasta que se mate el proceso)
     # busca cuantos archivos .txt existen en entrada para luego procesarlos y moverlos a procesado
     if ls ~/EPNro1/entrada/*.txt > /dev/null 2>&1; then # si existen los archivos ejecuta el bucle (/dev/null se deshace del output del comando para que no se printee en la terminal, mientras que 2>&1 se deshace de los outputs de errores para cuando no hay archivos en la carpeta)
